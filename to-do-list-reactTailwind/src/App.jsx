aa7b0aeb5c8786c238047497c6e060912e62f76d
fwhite <hola@ejemplo.com>
2023-10-01 10:29:50 -0300

    inicio repositorio

---
+++ to-do-list-reactTailwind/src/App.jsx
@@ -0,0 +1,14 @@
+import freecodecampImg from './assets/img/freecodecamp.png'
+
+function App() {
+  return (
+    <div id="aplicacion-tareas">
+      <div id="contenedor-img-freecodecamp">
+        <img src={freecodecampImg} alt="logo-freecodecamp" />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
